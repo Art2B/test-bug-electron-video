fcd1b3f535710344dc9b9aab4c59e9e463f8ee6e
Arthur Battut <arthur.battut@gmail.com>
2015-07-31 19:06:23 +0200

    modif on videos

--- app/data/videos.js
+++ app/data/videos.js
@@ -1,35 +1,33 @@
 var VideoDatas = [
   {
-    slug: "taylor_swift_bad_blood",
-    name: "Taylor Swift - Bad Blood ft. Kendrick Lamar",
-    file: "taylor_swift_bad_blood.mp4",
-    type: "video/mp4"
-  },
-  {
     slug: "ojectives",
     name: "Objectives Video",
-    file: "star_wars_7_trailer.mp4",
+    file: "HD_4_SUPERMARCHE_SCREEN_1.mp4",
     type: "video/mp4",
-    hide: true
   }, {
-    slug: "wiz_khalifa_see_you_again",
-    name: "Wiz Khalifa - See you again",
-    file: "wiz_khalifa_see_you_again.mp4",
+    slug: "parking_allee",
+    name: "Parking et Allée",
+    file: "HD_4_SUPERMARCHE_SCREEN_2.mp4",
     type: "video/mp4"
   }, {
-    slug: "david_guetta_hey_mama",
-    name: "David Guetta - Hey Mama (Official Video) ft Nicki Minaj, Bebe Rexha & Afrojack",
-    file: "david_guetta_hey_mama.mp4",
+    slug: "offre_solide_liquide",
+    name: "Offre solide et liquide",
+    file: "HD_4_SUPERMARCHE_SCREEN_3.mp4",
     type: "video/mp4"
   }, {
-    slug: "ellie_goulding_love_me_like_you_do",
-    name: "Ellie Goulding - Love Me Like You Do (Official Video)",
-    file: "ellie_goulding_love_me_like_you_do.mp4",
+    slug: "rayon_1",
+    name: "Rayon 1",
+    file: "HD_4_SUPERMARCHE_SCREEN_4.mp4",
     type: "video/mp4"
   }, {
-    slug: "marron_5_sugar",
-    name: "Maroon 5 - Sugar",
-    file: "marron_5_sugar.mp4",
+    slug: "rayon_2",
+    name: "Rayon 2",
+    file: "HD_4_SUPERMARCHE_SCREEN_5.mp4",
+    type: "video/mp4"
+  }, {
+    slug: "conso",
+    name: "Consommation Immédiate",
+    file: "HD_4_SUPERMARCHE_SCREEN_6.mp4",
     type: "video/mp4"
   },
 ];
