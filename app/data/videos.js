3f6af13cb62740de598a6b488ff1d36c3eb59f05
Arthur Battut <arthur.battut@gmail.com>
2015-08-25 11:25:17 +0200

    first commit

--- app/data/videos.js
+++ app/data/videos.js
@@ -3,49 +3,17 @@ var VideoDatas = [
     slug: "ojectives",
     name: "Objectives Video",
     files: {
-      en: "HD_4_SUPERMARCHE_SCREEN_1.mp4",
-      fr: "HD_4_SUPERMARCHE_SCREEN_1.mp4"
+      en: "objectifs.mp4",
+      fr: "objectifs.mp4"
     },
     type: "video/mp4",
   }, {
     slug: "parking_allee",
     name: "Parking et Allée",
     files: {
-      en: "HD_4_SUPERMARCHE_SCREEN_2.mp4",
-      fr: "HD_4_SUPERMARCHE_SCREEN_2.mp4"
+      en: "parking.mp4",
+      fr: "parking.mp4"
     },
     type: "video/mp4"
-  }, {
-    slug: "offre_solide_liquide",
-    name: "Offre solide et liquide",
-    files: {
-      en: "HD_4_SUPERMARCHE_SCREEN_3.mp4",
-      fr: "HD_4_SUPERMARCHE_SCREEN_3.mp4"
-    },
-    type: "video/mp4"
-  }, {
-    slug: "rayon_1",
-    name: "Rayon 1",
-    files: {
-      en: "HD_4_SUPERMARCHE_SCREEN_4.mp4",
-      fr: "HD_4_SUPERMARCHE_SCREEN_4.mp4"
-    },
-    type: "video/mp4"
-  }, {
-    slug: "rayon_2",
-    name: "Rayon 2",
-    files: {
-      en: "HD_4_SUPERMARCHE_SCREEN_5.mp4",
-      fr: "HD_4_SUPERMARCHE_SCREEN_5.mp4"
-    },
-    type: "video/mp4"
-  }, {
-    slug: "conso",
-    name: "Consommation Immédiate",
-    files: {
-      en: "HD_4_SUPERMARCHE_SCREEN_6.mp4",
-      fr: "HD_4_SUPERMARCHE_SCREEN_6.mp4"
-    },
-    type: "video/mp4"
-  },
+  }
 ];
